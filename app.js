68638a47cf1afb87cca4d854b5dbc4653209d4a2
rlaeorbszz <rlaeorbszz@gmail.com>
2026-02-04 02:30:22 +0900

    Mongo ID delete

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import Review from "./models/review.js";
 import { fileURLToPath } from 'url';
 
 
-const url = 'mongodb+srv://db_user:Dbdb1122@cluster0.guxh1eb.mongodb.net/yelpclone?retryWrites=true&w=majority'
+const url = '.mongodb.net/yelpclone?retryWrites=true&w=majority'
 
 mongoose.connect(url, {
     useNewUrlParser: true,
